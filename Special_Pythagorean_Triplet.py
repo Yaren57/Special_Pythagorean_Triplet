ab7b7c0297f9df877e60eea5abdf45b8c1414a89
Şirin Yaren Değerli <76749230+Yaren57@users.noreply.github.com>
2022-07-10 17:31:32 +0300

    Update Special_Pythagorean_Triplet.py

--- Special_Pythagorean_Triplet.py
+++ Special_Pythagorean_Triplet.py
@@ -1,10 +1,6 @@
-import math
-
-for a in range(1, 1000):
-    for b in range(1, 1000):
-        c = math.sqrt(a ** 2 + b ** 2)
-        if a + b + c == 1000:
-            x = a * b * c
-
-            break
-print(x)
+for a in range(1, 400):
+    for b in range(1, 400):
+        c = (1000 - a) - b
+        if a < b < c:
+            if a**2 + b**2 == c**2:
+                print(a * b * c)
